b0714aa402776e3a70285db07f05716e316bd7de
shababhsiddique <shabab.h.siddique@gmail.com>
2018-02-09 20:42:27 +0600

    removed all div css hide/show process and replace with ajax subpage dom replacing

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,44 +2,56 @@
 
 var wrapperMenu = document.querySelector('.navbar-toggler');
 
+
+
 wrapperMenu.addEventListener('click', function () {
     wrapperMenu.classList.toggle('open');
 });
 
 
 function home() {
-    
+
 //    $(".page-about").hide();
     $(".page-about").addClass('hidden');
-    
+
     $(".page-contact").addClass('hidden');
     $(".page-skills").addClass('hidden');
 
     $(".page-index").removeClass('hidden');
 //    $(".page-header").removeClass('.hidden');
-    window.history.pushState('page2', 'Title', 'http://localhost/theme.shababhs');
+    window.history.pushState('page2', 'Title', 'http://localhost/theme.shababhs/');
 
 }
 
 
 function about() {
 
-
 //    $(".page-index").hide();
-    $(".page-index").addClass('hidden');
+//    $(".page-index").addClass('hidden');
 
 //    $(".page-contact").hide();
-    $(".page-contact").addClass('hidden');
+//    $(".page-contact").addClass('hidden');
 
 //    $(".page-skills").hide();
-    $(".page-skills").addClass('hidden');
+//    $(".page-skills").addClass('hidden');
 
 
 //    $(".page-about").delay(1000).show();
-    $(".page-about").removeClass('hidden');
-    
-    $(".page-about").css("opacity",1);
-    
+//    $(".page-about").removeClass('hidden');
+
+
+    $.get("subpages/about.html", function (subpage) {
+        $("#domContainer").hide().html(subpage).fadeIn();
+    });
+
+//    $("#domContainer").load("subpages/about.html", function () {
+//        $("#domContainer").css("opacity",1);
+//    });
+
+
+
+
+
     window.history.pushState('page2', 'Title', 'http://localhost/theme.shababhs/about');
 }
 
@@ -47,15 +59,25 @@ function about() {
 function skills() {
 
 //    $(".page-header").addClass('hidden');
-    $(".page-index").addClass('hidden');
+//    $(".page-index").addClass('hidden');
 
-    $(".page-about").addClass('hidden');
-    $(".page-contact").addClass('hidden');
+//    $(".page-about").addClass('hidden');
+//    $(".page-contact").addClass('hidden');
 
 
-    $(".page-skills").removeClass('hidden');
-    makeSkillCloud();
+//    $(".page-skills").removeClass('hidden');
+    $.get("subpages/skills.html", function (subpage) {
+        $("#domContainer").hide().html(subpage).fadeIn();
+        makeSkillCloud();
+    });
+
+//    $("#domContainer").load("subpages/skills.html", function () {
+//        $("#domContainer").css("opacity", 1);
+//        makeSkillCloud();
+//    });
     
+
+
     window.history.pushState('page2', 'Title', 'http://localhost/theme.shababhs/skills');
 }
 
@@ -63,28 +85,29 @@ function skills() {
 function contact() {
 
 //    $(".page-header").addClass('hidden');
-    $(".page-index").addClass('hidden');
+//    $(".page-index").addClass('hidden');
+//
+//    $(".page-about").addClass('hidden');
+//    $(".page-skills").addClass('hidden');
+//
+//    $(".page-contact").removeClass('hidden');
 
-    $(".page-about").addClass('hidden');
-    $(".page-skills").addClass('hidden');
+    $.get("subpages/contact.html", function (subpage) {
+        $("#domContainer").hide().html(subpage).fadeIn();
+    });
 
-    $(".page-contact").removeClass('hidden');
-    
+
     window.history.pushState('page2', 'Title', 'http://localhost/theme.shababhs/contact');
 
 }
 
 $(window).on('load', function () {
     console.log("window load occured!");
-    
-    urlString = window.location.href;
-    var urlarray = urlString.split('/');
-    
-    var fn = urlarray[urlarray.length-1];
-    
-    if(fn)
-    window[fn]();
-    
+
+
+    if (fn)
+        window[fn]();
+
     $('.preloaded-hidden').css("opacity", 1);
     console.log("test");
 });
