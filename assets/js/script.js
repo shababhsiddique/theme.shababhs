9ecf186a2f8ffc758cbf7a995d6d4775a52a2e22
shababhsiddique <shabab.h.siddique@gmail.com>
2018-02-09 17:33:21 +0600

    dynamic page change and removed pageheader to general bootstrap grid

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,9 +4,6 @@ var $svg = $('svg').drawsvg({
 
         $("#svg3722").load("assets/sign-fill.svg", function () {
             console.log("SVG fetched?");
-//              $("#colored-sign").addClass("opa1");
-//              $("#colored-sign").removeClass("opa1");
-//              $("#colored-sign").addClass("opa1");
 
             $("#colored-sign").delay(200)
                     .queue(function (next) {
@@ -16,6 +13,11 @@ var $svg = $('svg').drawsvg({
 
                         $(this).addClass("opa1");
                         $(this).addClass("hoverstrokefix");
+                        
+                        $("path.hvr-rotate").mouseover(function(){
+                            $("path.hvr-rotate").parent().addClass("hvrtransitionfix");
+                        });
+                        
                     });
         });
 
@@ -27,15 +29,6 @@ function animate() {
 }
 
 animate();
-
-
-
-$(window).on('load', function () {
-    console.log("window load occured!");
-
-    $('.preloaded-hidden').css("opacity", 1);
-    console.log("test");
-});
 
 
 var wrapperMenu = document.querySelector('.navbar-toggler');
@@ -103,3 +96,10 @@ function contact() {
     $(".page-contact").removeClass('hidden');
 
 }
+
+$(window).on('load', function () {
+    console.log("window load occured!");
+
+    $('.preloaded-hidden').css("opacity", 1);
+    console.log("test");
+});
